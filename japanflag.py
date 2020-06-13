bbdda89d878bbbd86304aedd487f6474ff603157
IshaanSB <66012604+IshaanSB@users.noreply.github.com>
2020-06-13 14:11:10 +0300

    Add files via upload
    flag of japan... feel free to change it to another country's flag

---
+++ japanflag.py
@@ -0,0 +1,27 @@
+#japanflag
+
+import turtle
+
+t = turtle.Turtle()
+
+t.lt(180)
+t.fd(300)
+t.rt(90)
+t.fd(350)
+t.rt(90)
+t.fd(650)
+t.rt(90)
+t.fd(350)
+t.rt(90)
+t.fd(350)
+t.up()
+t.rt(90)
+t.setpos(125,175)
+t.color('red')
+t.begin_fill()
+t.pencolor('red')
+t.down()
+t.circle(100)
+t.hideturtle()
+t.end_fill()
+
