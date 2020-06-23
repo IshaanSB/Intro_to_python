a18a2cd04e410b3d0f8a1c21a56a126f1495e164
IshaanSB <66012604+IshaanSB@users.noreply.github.com>
2020-06-23 10:41:04 +0300

    Add files via upload
    prints the tables of 1-10,
    I could have made it better,
    but I did not since, i did not find any way to make it better. but, i will make it better shortly.

---
+++ 1-10 tables products.py
@@ -0,0 +1,11 @@
+#2) write a program to write table of 1 to 10.
+
+list_of_tables = [1,2,3,4,5,6,7,8,9,10]
+products = 0
+for x in list_of_tables:
+   
+    for y in range(1,11):
+        print('products_of %s is %s' %(x,products))
+        products = x * y
+    
+
