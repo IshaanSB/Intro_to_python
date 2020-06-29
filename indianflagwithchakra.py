a98abd66cf5ed5d46f8ba81876c6f9a2e27f8184
IshaanSB <66012604+IshaanSB@users.noreply.github.com>
2020-06-29 11:32:19 +0300

    Add files via upload
    Indian flag with chakra

---
+++ indianflagwithchakra.py
@@ -0,0 +1,147 @@
+#indianflag.py
+
+import turtle
+t = turtle.Turtle()
+t.color("orange")
+t.begin_fill()
+t.pencolor('orange')
+t.lt(180)
+t.fd(300)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(450)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(150)
+t.end_fill()
+t.penup()
+t.fd(300)
+t.lt(90)
+t.pendown()
+t.color("black")
+t.pencolor("black")
+t.fd(100)
+t.lt(90)
+t.fd(450)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(450)
+t.penup()
+t.lt(90)
+t.fd(100)
+t.color("green")
+t.begin_fill()
+t.pencolor("green")
+t.pendown()
+t.fd(100)
+t.lt(90)
+t.fd(450)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(450)
+t.end_fill()
+t.penup()
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(225)
+t.color("blue")
+t.pencolor("blue")
+t.rt(90)
+t.fd(30)
+t.penup()
+t.setpos(-105,-50)
+
+t.down()
+t.color('blue')
+t.pensize(5)
+
+t.circle(45)
+t.up()
+t.pensize(0)
+t.down()
+t.begin_fill()
+t.up()
+t.setpos(-72,-50)
+t.circle(10)
+t.end_fill()
+
+
+for x in range(1,25):
+    t.color('red')
+    t.up()
+    t.rt(85+20)
+    t.color('blue')
+    t.down()
+    t.pensize(0)
+    
+    
+    t.setpos(-60,-53)
+    t.fd(35+10)
+    t.color('red')
+    t.up()
+    t.hideturtle()
+    
+
+
+
+
+
+
+
+
+
+
+
+'''
+t.pendown()
+t.circle(45)
+t.lt(90)
+t.penup()
+t.fd(50)
+t.penup()
+t.setpos(-60,-90)
+t.pendown()
+t.color("blue")
+t.begin_fill()
+t.circle(40)
+t.hideturtle()
+t.end_fill()
+'''
+
+
+#t.color('black')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+   
+
+
+
+
+
+
+
