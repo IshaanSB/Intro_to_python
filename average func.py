0c91f68ba29921d51050bec719eb211160573fdf
IshaanSB <66012604+IshaanSB@users.noreply.github.com>
2020-07-03 13:18:34 +0300

    Add files via upload
    it does the average of any amount of numbers. it is not a normal average program but a function, it can be used by users to get the averages of numbers

---
+++ average func.py
@@ -0,0 +1,29 @@
+#averageofnums
+
+def avg(list_b):
+    for x in range(2,10):
+        for i in list_b:
+            if x > 10:
+                break
+            elif x < 10:
+               y = len(list_b)
+            for x in list_b:
+                for n in range(0,y):
+                   Sum = sum(list_b)
+                   return Sum / y
+                   
+                
+           
+            
+            
+            
+r = [10,20]
+
+print(avg(r))
+        
+        
+        
+            
+    
+    
+    
