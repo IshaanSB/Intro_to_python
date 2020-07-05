2d7735debd2d4b6abce2c9e54f5158ccbd0ec00f
IshaanSB <66012604+IshaanSB@users.noreply.github.com>
2020-07-05 12:31:44 +0300

    Add files via upload
    prints the sum of numbers in a list just like the normal sum func

---
+++ ownsum.py
@@ -0,0 +1,30 @@
+r = [10,10,20]
+
+num = print(sum(r))
+
+import sys
+
+
+def own_sum(list_x):
+    y = len(list_x)
+    Sum = 0
+    for r in list_x:
+       if y > 11:
+            sys.exit
+    else:
+        for v in list_x:
+            Sum = Sum + v
+        return Sum
+               
+          
+    
+        
+        
+       
+    
+
+n = [7,9,9,7,7,7]
+
+print(own_sum(n))
+            
+        
