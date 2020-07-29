1dc987e7d0c56ac6ccd32711b038566a6cbbe963
IshaanSB <66012604+IshaanSB@users.noreply.github.com>
2020-07-29 13:02:01 +0300

    Add files via upload
    It tells the user the name of the element and its atomic mass and number , working on it so it works for compounds too!!! Will add more elements also

---
+++ Detector.py
@@ -0,0 +1,49 @@
+#dict that contains element symbols
+symbols_dict = {'H' : '1' , 'He' : '2' , 'Li' : '3' , 'Be' :'4' , 'B' : '5' , 'C' : '6' , 'N' : '7' , 'O' : '8'}
+
+print(symbols_dict)
+
+symbol_input = input('Choose one of the elements form the dict:')
+
+atomic_mass_dict = {'H' : '1' , 'He' : '4' , 'Li' : '7' , 'Be' : '9' , 'B' : '11' , 'C' : '12' , 'N' : '14' , 'O' : '16'}
+
+names_of_element_dict = {'H' : 'Hydrogen' , 'He' : 'Helium' , 'Li' : 'Lithium' , 'Be' : 'Beryllium' , 'B' : 'Boron' , 'C' : 'Carbon' , 'N' : 'Nitrogen' , 'O' : 'Oxygen'}
+
+input_symbol = symbol_input
+
+for y in symbols_dict:
+    if symbol_input == y:
+        print('atomic number is:-')
+        print(symbols_dict[y])
+        break
+
+else:
+    for x in symbol_input:
+        if x in symbols_dict:
+            print(symbols_dict[x])
+            break
+           
+for a in atomic_mass_dict:
+    print('atomic mass is:-')
+    print(atomic_mass_dict[input_symbol])
+    break
+
+
+
+
+
+
+for r in names_of_element_dict:
+    if symbol_input == r:
+        print(names_of_element_dict[r])
+        break
+    else:
+        for b in symbol_input:
+            if b in names_of_element_dict:
+                break
+                print(names_of_element_dict[b])
+                
+
+
+   
+    
